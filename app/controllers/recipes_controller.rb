ad63c03c1efb0477213ff364f45d5911be5cc45a
Felix <barosiofelix@gmail.com>
2022-12-27 20:57:59 +0300

    add: create & update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
         render json: Recipe.all
@@ -11,14 +12,33 @@ class RecipesController < ApplicationController
         render json: recipe, status: :ok
     end
 
+    def create
+        recipe = Recipe.create!(recipe_params)
+        render json: recipe, status: :created
+    end
+
+    def update
+        recipe = find_recipe
+        recipe.update!(recipe_params)
+        render json: recipe, status: :ok
+    end
+
     private
 
     def find_recipe
         recipe = Recipe.find(params[:id])
     end
 
+    def recipe_params
+        params.permit(:meal_name, :image_url, :ingredients, :instructions, :origin, :category, :tags, :video)
+    end
+
     def render_not_found_response
         render json: { errors: "Recipe not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end

603e4e1485fd89c848ef2bd242b06c9c50f9a5ab
Felix <barosiofelix@gmail.com>
2022-12-28 16:30:13 +0300

    update

---
+++ client/src/components/Post.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+
+const api = "/recipes";
+
+const Post = ({ posts, loadPosts }) => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Post;

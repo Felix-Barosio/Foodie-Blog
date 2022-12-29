1a1ecfa122b0ef6053b398ee034924e83c55f91c
Felix <barosiofelix@gmail.com>
2022-12-29 10:48:36 +0300

    update

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
 
 const api = "/recipes";
 
+// default states
+const initialState = {
+    meal_name: "",
+    image_url: "",
+    instructions: "",
+    origin: "",
+    tags: "",
+    category: "",
+    video: "",
+    ingredients: "",
+};
+
 const Post = ({ posts, loadPosts }) => {
+    const [show, setShow] = useState(false);
+    const [state, setState] = useState(initialState);
+    const [userId, setUserId] = useState(null);
+    const [editMode, setEditMode] = useState(false);
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredResults, setFilteredResults] = useState([]);
+
     return (
         <div>
         </div>

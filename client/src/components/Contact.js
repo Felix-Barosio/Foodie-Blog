a805507fb1a44c442e849be0f2d346b00948774a
Felix Barosio <barosiofelix@gmail.com>
2022-12-31 09:38:45 +0300

    update

---
+++ client/src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import ContactData from './ContactData'
+
+function Contact() {
+    return (
+        <div>
+            <div className='contact-us'>
+                <div className='contact-message'>
+                    <div className='message'>
+                        <h1>Contact Me.</h1>
+                        <p>Need to get in touch with us? <br />
+                            Fill in the inquiry form below.
+                        </p>
+                    </div>
+                </div>
+                <div className='contact-form'>
+                    {/* <ContactData /> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact

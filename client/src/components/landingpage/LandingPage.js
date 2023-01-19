6ade5a379fe0b13b9165dfa89a29a3580211c34e
Felix <barosiofelix@gmail.com>
2023-01-19 07:36:50 +0300

    add: home

--- client/src/components/landingpage/LandingPage.js
+++ client/src/components/landingpage/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import HomePage from './homepage/HomePage'
 import NavBar from './navbar/NavBar'
-
 
 function LandingPage() {
   return (
     <div>
       <NavBar />
+      <HomePage />
     </div>
   )
 }

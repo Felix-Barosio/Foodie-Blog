923b297b06997a9e9f2f9ec10a83f411217efa44
Felix <barosiofelix@gmail.com>
2023-01-20 10:56:33 +0300

    update

--- client/src/components/landingpage/LandingPage.js
+++ client/src/components/landingpage/LandingPage.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import HomePage from './homepage/HomePage'
 import NavBar from './navbar/NavBar'
+import About from './about/About'
+import Partners from './partners/OurPartners'
+import Footer from './footer/Footer'
+import Services from './ourservices/Services'
 
 function LandingPage() {
   return (
     <div>
       <NavBar />
       <HomePage />
+      <About />
+      <Services />
+      <Partners />
+      <Footer />
     </div>
   )
 }
